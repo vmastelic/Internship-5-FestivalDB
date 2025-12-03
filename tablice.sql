9480ca8dcc61e03c0479d382c4c7760a7995e9cf
vmastelic <vatroslav.mastelic@gmail.com>
2025-12-03 14:07:56 +0100

    Add artist table.

--- tablice.sql
+++ tablice.sql
@@ -9,10 +9,26 @@ CREATE TABLE Festivals(
 	HasCamp BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE Stages(
-	StageId SERIAL PRIMARY KEY,
-	FestivalId INT NOT NULL REFERENCES Festivals(FestivalId),
-	Location VARCHAR(20) NOT NULL CHECK(Location IN('main', 'forest', 'beach')),
-	MaxCapacity INT CHECK(MaxCapacity > 0),
-	Covered BOOLEAN DEFAULT FALSE
+CREATE TABLE Stage (
+    StageId SERIAL PRIMARY KEY,
+    FestivalId INT NOT NULL REFERENCES Festivals(FestivalId),
+    Name VARCHAR(30) NOT NULL,
+    Location VARCHAR(20) NOT NULL CHECK (Location IN ('main', 'forest', 'beach')),
+    MaxCapacity INT CHECK (MaxCapacity > 0),
+    Covered BOOLEAN DEFAULT FALSE,
+    UNIQUE (FestivalId, Name)
 );
+
+CREATE TABLE Artists(
+	ArtistId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL,
+	Country VARCHAR(30) NOT NULL,
+	Genre VARCHAR(30) NOT NULL,
+	MembersCount INT CHECK(MembersCount > 0),
+	IsActive BOOLEAN DEFAULT TRUE
+);
+
+
+
+
+
